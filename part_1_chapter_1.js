baa86c6737f71c6aa15677c904f1eccc9d3c7ce6
Natalya Myunster <natalyamyunster@yahoo.com>
2020-07-09 11:26:18 +0200

    Looping a triangle

---
+++ part_1_chapter_1.js
@@ -0,0 +1,17 @@
+//Looping a Triangle
+const loopingTriangle = () => {
+    let res = '';
+    for(let i = 0; i < 7 ; i++){
+
+        res += '#';
+        console.log(res);
+
+    }
+}
+
+loopingTriangle();
+
+console.log('========================');
+
+//FizzBuzz_1
+

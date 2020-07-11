86c2b418e7c80e20ba2496a6d955929028a38832
Natalya Myunster <natalyamyunster@yahoo.com>
2020-07-11 14:23:07 +0200

    FizzBuzz

--- part_1_chapter_1.js
+++ part_1_chapter_1.js
@@ -1,7 +1,7 @@
 //Looping a Triangle
 const loopingTriangle = () => {
     let res = '';
-    for(let i = 0; i < 7 ; i++){
+    for (let i = 0; i < 7; i++) {
 
         res += '#';
         console.log(res);
@@ -13,5 +13,26 @@ loopingTriangle();
 
 console.log('========================');
 
-//FizzBuzz_1
+//FizzBuzz
+
+const FizzBuzz = () => {
+    for (let i = 1; i <= 100; i++) {
+        if (i % 15 === 0) {
+            console.log('FizzBuzz');
+        } else if (i % 3 === 0) {
+            console.log('Fizz');
+        } else if (i % 5 === 0) {
+            console.log('Buzz')
+        } else {
+            console.log(i);
+        }
+    }
+}
+
+
+FizzBuzz();
+
+//Chessboard
+
+
 

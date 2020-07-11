4e8c4f98891771cd414fc5b28fdb67dff52fef7c
Natalya Myunster <natalyamyunster@yahoo.com>
2020-07-11 15:59:08 +0200

    chessboard(8);

--- part_1_chapter_2.js
+++ part_1_chapter_2.js
@@ -34,5 +34,22 @@ FizzBuzz();
 
 //Chessboard
 
+const chessboard = (size) => {
 
+    let board = "";
+
+    for (let y = 0; y < size; y++) {
+        for (let x = 0; x < size; x++) {
+            if ((x + y) % 2 === 0) {
+                board += " ";
+            } else {
+                board += "#";
+            }
+        }
+        board += "\n";
+    }
+
+    console.log(board);
+}
+chessboard(8);
 

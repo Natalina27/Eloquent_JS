b55653b52747b4ef4b5ae9eb57c76766182dc8e0
Natalya Myunster <natalyamyunster@yahoo.com>
2020-10-14 00:28:47 +0200

    refactoring

---
+++ Part_1/Chapter_3/3.2_Recursion.js
@@ -0,0 +1,6 @@
+console.log('======3.2 Recursion ========');
+
+
+//Recursion
+// Recursive function isEven accepts 1 parameter - positive whole number - and return Boolean
+//test - 50, 75, -1
--- Part_1/Chapter_3/part_1_chapter_3.js
+++ Part_1/Chapter_3/part_1_chapter_3.js
@@ -5,9 +5,7 @@
 
 console.log('========================');
 
-//Recursion
-// Recursive function isEven accepts 1 parameter - positive whole number - and return Boolean
-//test - 50, 75, -1
+
 
 
 console.log('========================');

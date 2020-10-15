88c90e8b908c4da3e9628fb5c3590fe6beab5459
Natalya Myunster <natalyamyunster@yahoo.com>
2020-10-15 22:33:45 +0200

    refactoring

--- Part_1/Chapter_3/3.3_BeanCounting.js
+++ Part_1/Chapter_3/3.3_BeanCounting.js
@@ -1,17 +1,6 @@
-//Part1
-//3.Functions
-
-
-
-console.log('========================');
-
-
-
-
-console.log('========================');
+console.log('======3.3 Bean Counting ========');
 
 //Bean counting
 // 1.Function countBs takes a string and returns a number of uppercase B
 // 2. Function countChar that behaves like countBs but it takes 2 arguments strind and character to count
 // 3. Rewrite countBs to make use of this new function
-

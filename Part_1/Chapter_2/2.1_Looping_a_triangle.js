fafd25a2b0a7c5a7f41dee7568121d4edceeca2c
Natalya Myunster <natalyamyunster@yahoo.com>
2020-10-17 11:00:49 +0200

    3.1 Minimum

--- Part_1/Chapter_2/2.1_Looping_a_triangle.js
+++ Part_1/Chapter_2/2.1_Looping_a_triangle.js
@@ -9,10 +9,8 @@ console.log('====== 2.1 Looping a Triangle========');
 const loopingTriangle = () => {
   let res = '';
   for (let i = 0; i < 7; i++) {
-
     res += '#';
     console.log(res);
-
   }
 }
 
--- Part_1/Chapter_3/3.1_Minimum.js
+++ Part_1/Chapter_3/3.1_Minimum.js
@@ -2,3 +2,13 @@ console.log('======3.1 Minimum========');
 
 //Minimum
 // Function takes 2 arguments and return min
+const minimun = (arg1, arg2) =>{
+  if(arg1 === arg2) {return 'the args are the same'}
+  let min = arg1;
+  min = arg1 < arg2 ? arg1 : arg2;
+  return min;
+}
+
+console.log(minimun(2,3)); //2
+console.log(minimun(8,5)); //5
+console.log(minimun(4,4)); //2

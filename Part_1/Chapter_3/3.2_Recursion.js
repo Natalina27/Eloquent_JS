12bffa830b0fe5a180ed6b423700819a2113b64d
Natalya Myunster <natalyamyunster@yahoo.com>
2020-10-18 08:25:26 +0200

    3.2 Recursion

--- Part_1/Chapter_3/3.2_Recursion.js
+++ Part_1/Chapter_3/3.2_Recursion.js
@@ -3,4 +3,23 @@ console.log('======3.2 Recursion ========');
 
 //Recursion
 // Recursive function isEven accepts 1 parameter - positive whole number - and return Boolean
+//0 - even
+//1 - odd
+// n-2
 //test - 50, 75, -1
+
+const isEven = (number) => {
+  if( number === 0 ) return true;
+  if( number === 1 ) return false;
+  if (number < 0){
+    return isEven(number + 2)
+  } else{
+    return isEven(number - 2);
+  }
+}
+
+console.log(isEven(-50)); //true
+console.log(isEven(75)); //false
+console.log(isEven(-1)); //false
+console.log(isEven(0)); //true
+

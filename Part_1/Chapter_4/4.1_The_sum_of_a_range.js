79550fb017a37666e06710d7ae4f90f3793ebbf8
Natalya Myunster <natalyamyunster@yahoo.com>
2020-10-31 08:57:11 +0100

    added Chapter_4 4.1 task description

--- Part_1/Chapter_4/4.1_The_sum_of_a_range.js
+++ Part_1/Chapter_4/4.1_The_sum_of_a_range.js
@@ -1,1 +1,9 @@
 console.log('======4.1 ========');
+//1. Write a range function that takes three arguments,
+// start , end and step, and return an array containing
+// all the numbers from start up to end (including)
+
+//2. Write a sum function that takes an array of numbers and return the sum of these numbers
+
+
+

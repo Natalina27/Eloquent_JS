fbf04aff276f563fbb15683845e458124a547942
Natalya Myunster <natalyamyunster@yahoo.com>
2020-11-03 11:18:37 +0100

    4.2 added task instructions

--- Part_1/Chapter_4/4.2_Reversing_an_array.js
+++ Part_1/Chapter_4/4.2_Reversing_an_array.js
@@ -1,1 +1,9 @@
 console.log('======4.2 ========');
+// Write 2 functions reverseArray and reverseArrayInPlace.
+//The first reverseArray takes an array as argument and produces a new array
+// that has the same elements in the inverse order.
+//The second modifies an array given as argument by reversing its elements
+
+//1.reverseArray
+
+//2. reverseArrayInPlace

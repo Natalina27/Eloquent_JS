5f842663acd26e509461b79685a1b300bce1cb63
Natalya Myunster <natalyamyunster@yahoo.com>
2020-11-08 11:50:45 +0100

    4.3 added instructions

--- Part_1/Chapter_4/4.3_A_list.js
+++ Part_1/Chapter_4/4.3_A_list.js
@@ -1,1 +1,23 @@
 console.log('======4.3 ========');
+/*
+Write a fiunction arrayToList that builds up a list structure
+like the one shown when given [1, 2, 3] as argument.
+```
+  let list = {
+    value: 1.
+    rest : {
+      value: 2,
+      rest : {
+        value : 3,
+        rest: null
+      }
+    }
+  };
+```
+  Also write a listArray function that produces an array from a list.
+  Then add a helper function prepend, which takes an element and a list
+and creates a new list that add the element to the front on the input list.
+and nth that takes a list and a number and returns the element at the given position
+in the list or undefined when there is no such element.
+If you haven't already , also write a recursive version  of nth.
+ */

de0af815fa34194d428980b6e9180568f5e1411c
Natalya Myunster <natalyamyunster@yahoo.com>
2020-11-13 08:46:00 +0100

    4.3 prepend function

--- Part_1/Chapter_4/4.3_A_list.js
+++ Part_1/Chapter_4/4.3_A_list.js
@@ -60,3 +60,12 @@ const listArray = (list) =>{
 };
 console.log(listArray(list)); //[1, 2, 3]
 
+//3. prepend
+function prepend(value, list) {
+  return {value, rest: list};
+}
+
+console.log(prepend(10, prepend(20, null)));
+/*
+{value: 10, rest: {value: 20, rest: null}}
+ */
